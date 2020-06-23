687531e759938ce8b2c486042361a99882d4636c
Ivan Pyrog <ipirognoe@gmail.com>
2020-06-23 18:13:09 +0300

    [6th commit] finished the output; struggling with the 2nd case page

--- pyrog_ivan_agile_engine_test.py
+++ pyrog_ivan_agile_engine_test.py
@@ -3,38 +3,28 @@ import sys
 from bs4 import BeautifulSoup
 
 # Lets declare variables to contain urls to our web-sites:
-"""input_origin_file_path = sys.argv[1]"""
-"""input_other_sample_file_path = sys.argv[2]"""
-
-input_origin_file_path = "https://agileengine.bitbucket.io/beKIvpUlPMtzhfAy/samples/sample-0-origin.html"
-input_other_sample_file_path_1 = "https://agileengine.bitbucket.io/beKIvpUlPMtzhfAy/samples/sample-1-evil-gemini.html"
-input_other_sample_file_path_2 = "https://agileengine.bitbucket.io/beKIvpUlPMtzhfAy/samples/sample-2-container-and-clone.html"
-input_other_sample_file_path_3 = "https://agileengine.bitbucket.io/beKIvpUlPMtzhfAy/samples/sample-3-the-escape.html"
-input_other_sample_file_path_4 = "https://agileengine.bitbucket.io/beKIvpUlPMtzhfAy/samples/sample-4-the-mash.html"
-
+input_origin_file_path = sys.argv[1]
+input_other_sample_file_path = sys.argv[2]
 
 # Lets declare variables to contain request results (GET method):
 origin_page = requests.get(input_origin_file_path)
-other_page_1 = requests.get(input_other_sample_file_path_1)
-other_page_2 = requests.get(input_other_sample_file_path_2)
-other_page_3 = requests.get(input_other_sample_file_path_3)
-other_page_4 = requests.get(input_other_sample_file_path_4)
-
+other_page = requests.get(input_other_sample_file_path)
 
 # Lets make the Soup:
-origin_page_soup = BeautifulSoup(origin_page.text,  features="lxml")
-other_page_soup_1 = BeautifulSoup(other_page_1.text, features="lxml")
-other_page_soup_2 = BeautifulSoup(other_page_2.text, features="lxml")
-other_page_soup_3 = BeautifulSoup(other_page_3.text, features="lxml")
-other_page_soup_4 = BeautifulSoup(other_page_4.text, features="lxml")
+origin_page_soup = BeautifulSoup(origin_page.text, features="lxml")
+other_page_soup = BeautifulSoup(other_page.text, features="lxml")
 
-button_search_result_list = other_page_soup_4.find_all('a', attrs={"class": "btn btn-success", "onclick": True})
+button_origin_search_result_list = origin_page_soup.find_all('a', attrs={"class": "btn btn-success", "onclick": True})
+button_other_search_result_list = other_page_soup.find_all('a', attrs={"class": "btn btn-success", "onclick": True})
+
 
 def xpath_builder(search_results_tags):
-    """Function to assemble Xpath of the given list of Beautiful Soup tags found"""
+    """Function to assemble Xpath of the given list of Beautiful Soup tags found
+    :type search_results_tags: list
+    """
     output = ""
     for item in search_results_tags:
-        xml_elements_list =[]
+        xml_elements_list = []
         for parent in item.parents:
             xml_elements_list.append(parent.name)
         xml_elements_list.reverse()
@@ -42,4 +32,5 @@ def xpath_builder(search_results_tags):
     return output
 
 
-print(xpath_builder(button_search_result_list))
+print(f"The original page Xpath: {xpath_builder(button_origin_search_result_list)}")
+print(f"The updated page Xpath: {xpath_builder(button_other_search_result_list)}")
